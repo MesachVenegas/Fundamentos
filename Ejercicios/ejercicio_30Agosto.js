c752d1eeeb3536bbf0d4f7448948ee3892898a11
Mesach Venegas <mesach.venegas@hotmail.com>
2022-08-31 17:09:57 -0700

    add: Ejercicios de practica clase 31Agosto

---
+++ Ejercicios/ejercicio_30Agosto.js
@@ -0,0 +1,29 @@
+/*
+Acá un pequeño ejercicio para ver mas combinaciones de operaciones entre distintos tipos de dato.
+La idea es que antes de que ejecuten el archivo para visualizar el console.log() intenten determinar cual será el resultado a partir de lo visto en clase. Pueden escribir el resultado que ustedes prevean como un comentario en linea después del console.log()
+*/
+
+
+console.log(2 + '2') //4
+
+//Recuerda que cuando tenemos mas de una operación envuelta se involucra la jerarquía de operaciones.
+//Cuando tenemos la misma operación (en este caso un +), entonces las operaciones se ejecutan de izquierda a derecha
+/*
+console.log(1 + 1 + '2') //
+console.log('2' + 1 + 1) //
+console.log(true + true) //
+console.log(true / null) //
+console.log('20' - true) //
+console.log(true + '20') //
+console.log(25 + undefined) //
+
+//¿Qué hace en este caso el operador +?
+//Ejecuta el console.log() y a partir del resultado saca tus conclusiones
+console.log( +true ) //
+
+console.log(5 > 4) //
+console.log('banco' < 'barco') //
+console.log('2' > '12') //
+console.log(0 === 1) //
+console.log(1 !== 0) //
+*/
