a8cdf0f89618c692f72b32e73cc2f4e4815d5efb
Mesach Venegas <mesach.venegas@hotmail.com>
2022-09-22 01:06:01 -0700

    add: solución ejercicio 18 de replit

--- Ejercicios/test.js
+++ Ejercicios/test.js
@@ -315,7 +315,7 @@ function countLetter(phrase, letter){
     }
     return counter;
 }
-
+4
 // console.log(countLetter(phrase, letter))
 
 
@@ -377,3 +377,22 @@ function countApplicationsBychannel(students){
 }
 
 // console.log(countApplicationsBychannel(students_social));
+
+
+/*
+    Encuentra la cantidad de números individuales que componen a un número
+    La función countNumbers() recibe como parámetro un número
+
+    Tu labor es encontrar y retornar el total de números individuales que lo componen.
+    Ejemplo:
+        Si la función recibe el número 128 debemos retornar un 3 porque el número 128 está compuesto por 3 números.
+    Ejemplo 2:
+        Si la función recibe el número 84 debemos retornar un 2 porque el número 84 está compuesto por 2 números.
+    - Recuerda utilizar return para devolver tu solución.
+    - El número que recibe la función puede ser cualquiera.
+*/
+
+function countNumbers(number){
+    return String(number).length
+}
+
